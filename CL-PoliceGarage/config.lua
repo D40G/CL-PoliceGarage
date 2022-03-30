444de08586d53fd18ac2a420531ffd5593fb6d3a
NevoSwissa <96447671+NevoSwissa@users.noreply.github.com>
2022-03-31 01:27:05 +0300

    Add files via upload

---
+++ CL-PoliceGarage/config.lua
@@ -0,0 +1,30 @@
+Config = Config or {}
+
+Config.UseColors = false
+
+Config.UseMarkerInsteadOfMenu = false -- Want to use the marker to return the vehice? if false you can do that by opening the menu
+
+Config.MS = 'high' -- MS for the script recommended using high if not the "close" will get a bit baggy. options high / low
+
+Config.Job = 'police'
+
+--You Can Add As Many As You Like
+--DO NOT add vehicles that are not in your shared ! otherwise the qb-garages wont work
+Config.Vehicles = {
+    [1] = {
+        ['vehiclename'] = "Bati", --Name
+        ['vehicle'] = "bati", --Model
+        ['price'] = 5000, --Price
+        ['r'] = 0, --Vehicle Color (optional)
+        ['g'] = 0,
+        ['b'] = 0,
+    }, 
+    [2] = {
+        ['vehiclename'] = "Test", --Name
+        ['vehicle'] = "t20", --Model
+        ['price'] = 18000, --Price
+        ['r'] = 0, --Vehicle Color (optional)
+        ['g'] = 0,
+        ['b'] = 0,
+    }, 
+}
