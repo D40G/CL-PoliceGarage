dd441d4d04aa805f71567bdfe834b7422357a600
NevoSwissa <96447671+NevoSwissa@users.noreply.github.com>
2022-03-31 18:13:13 +0300

    Add files via upload

---
+++ CL-PoliceGarage/CL-PoliceGarage/config.lua
@@ -0,0 +1,50 @@
+Config = Config or {}
+
+-- Dont forgot to setup the discord logs via the server.lua
+
+Config.UseColors = false
+
+Config.UseMarkerInsteadOfMenu = false -- Want to use the marker to return the vehice? if false you can do that by opening the menu
+
+Config.SetVehicleTransparency = 'low' -- Want to make the vehicle more transparent? you have a lot of options to choose from: low, medium, high, none
+
+Config.MS = 'high' -- MS for the script recommended using high if not the "close" will get a bit baggy. options high / low
+
+Config.Job = 'police'
+
+--You Can Add As Many As You Like
+--DO NOT add vehicles that are not in your shared ! otherwise the qb-garages wont work
+Config.Vehicles = {
+    [1] = {
+        ['vehiclename'] = "Bati", --Name
+        ['vehicle'] = "bati", --Model
+        ['price'] = 5000, --Price
+        ['r'] = 0, --Vehicle Color (Optional)
+        ['g'] = 0,
+        ['b'] = 0,
+    }, 
+    [2] = {
+        ['vehiclename'] = "Test", --Name
+        ['vehicle'] = "t20", --Model
+        ['price'] = 18000, --Price
+        ['r'] = 0, --Vehicle Color (Optional)
+        ['g'] = 0,
+        ['b'] = 0,
+    }, 
+    [3] = {
+        ['vehiclename'] = "Police2", --Name
+        ['vehicle'] = "sultan", --Model
+        ['price'] = 10000, --Price
+        ['r'] = 0, --Vehicle Color (Optional)
+        ['g'] = 0,
+        ['b'] = 0,
+    }, 
+    [4] = {
+        ['vehiclename'] = "SultanRS", --Name
+        ['vehicle'] = "sultanrs", --Model
+        ['price'] = 52000, --Price
+        ['r'] = 0, --Vehicle Color (Optional)
+        ['g'] = 0,
+        ['b'] = 0,
+    }, 
+}
